dee62d3db977ce504d074c119236b0b1afe28346
minh <minhnguyengit@gmail.com>
2018-11-15 11:24:02 +0700

    Client Post Create user

--- src/component/am_storeinfo.jsx
+++ src/component/am_storeinfo.jsx
@@ -5,18 +5,54 @@ import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button,
   Modal, ModalHeader, ModalBody, ModalFooter,
-  Form, FormGroup, Label, Input, FormFeedback, FormText
+  Form, FormGroup, Label, Input, Alert
 } from 'reactstrap'
 import AmsiTable from './am_si_table'
+import axios from 'axios'
+import {api} from '../config'
 
 class AmStoreinfo extends Component {
   constructor(props) {
     super(props)
-    this.state = { modal: false }
+    this.state = { modal: false, username: '', password: '', pwconfirm: '', email: '', label: false, message: '' }
     this.toggle = this.toggle.bind(this)
+    this.onChangeHandler = this.onChangeHandler.bind(this)
+    this.submitServer = this.submitServer.bind(this)
   }
   toggle() {
-    this.setState({ modal: !this.state.modal })
+    this.setState({ modal: !this.state.modal, username: '', password: '', pwconfirm: '', email: '', label: false, message: '' })
+  }
+  onChangeHandler(e) {
+    let name = e.target.name
+    let value = e.target.value
+    this.setState({ [name]: value })
+    let pwconfirm = this.state.pwconfirm
+    let pass = this.state.password
+    if (name === 'password' && value === pwconfirm || name === 'pwconfirm' && value === this.state.password) {
+      this.setState({ label: false })
+    } else if (this.state.password.length === 0 && this.state.pwconfirm.length === 0) {
+      this.setState({ label: false })
+    } else {
+      this.setState({ label: true, message: 'Password with Password Confirm not match' })
+    }
+  }
+  submitServer() {
+    let self = this
+    if (!this.state.label) {
+      let {username, password, pwconfirm, email} = this.state
+      let id_group = '5bd2de667496b64ea0b41684'
+      let id_roles = '5bd2de667496b64ea0b41682'
+      axios.post(api.local +'/api/AccountCr',{username, password, pwconfirm, email, id_group, id_roles})
+      .then(response => {
+        if (response.data.status===200) {
+          self.setState({modal: false})
+        }
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   }
   render() {
     return (
@@ -35,7 +71,7 @@ class AmStoreinfo extends Component {
             </CardText>
           </CardBody>
         </Card>
-        <Col md={{ size: 7 }} xs={{ size: 7 }} sm={{ size: 7 }}>
+        <Col md={{ size: 5 }} xs={{ size: 5 }} sm={{ size: 5 }}>
           <Row>
             <Col md={8}>
               <h1>Nhi Cosmetic Group</h1>
@@ -45,7 +81,7 @@ class AmStoreinfo extends Component {
             </Col>
           </Row>
           <br />
-          <Modal size='lg' centered='true' isOpen={this.state.modal}>
+          <Modal size='lg' centered={true} isOpen={this.state.modal}>
             <ModalHeader>
               Create Account Staff
             </ModalHeader>
@@ -54,33 +90,34 @@ class AmStoreinfo extends Component {
                 <FormGroup row>
                   <Label for='username' sm={3}>Username</Label>
                   <Col sm={8}>
-                    <Input type='username' name='username' id='username' placeholder='Your User' />
+                    <Input type='username' value={this.state.username} onChange={this.onChangeHandler} name='username' id='username' placeholder='Your User' />
                   </Col>
                 </FormGroup>
                 <FormGroup row>
                   <Label for='email' sm={3}>Email</Label>
                   <Col sm={8}>
-                    <Input type='email' name='email' id='exampleEmail' placeholder='Your email' />
+                    <Input type='email' value={this.state.email} onChange={this.onChangeHandler} name='email' id='email' placeholder='Your email' />
                   </Col>
                 </FormGroup>
                 <FormGroup row>
                   <Label for='password' sm={3}>Password</Label>
                   <Col sm={8}>
-                    <Input type='password' name='password' id='password' placeholder='Password' />
+                    <Input type='password' value={this.state.password} onChange={this.onChangeHandler} name='password' id='password' placeholder='Password' />
                   </Col>
                 </FormGroup>
                 <FormGroup row>
                   <Label for='password' sm={3}>Password Confirm</Label>
                   <Col sm={8}>
-                    <Input type='password' name='pwconfirm' id='pwconfirm' placeholder='Pass confirm' />
+                    <Input type='password' value={this.state.pwconfirm} onChange={this.onChangeHandler} name='pwconfirm' id='pwconfirm' placeholder='Pass confirm' />
                   </Col>
                 </FormGroup>
+                {this.state.label && <Alert color='danger'>{this.state.message}</Alert>}
               </Form>
             </ModalBody>
             <ModalFooter>
               <Row>
                 <Col md={5}>
-                  <Button onClick={this.toggle} color='success'>Save</Button>
+                  <Button onClick={this.submitServer} color='success'>Save</Button>
                 </Col>
                 <Col md={5}>
                   <Button onClick={this.toggle} color='danger'>Cancel</Button>
