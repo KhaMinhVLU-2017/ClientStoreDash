d6497004bcd1524f20c373a5804b00e4f35045d1
minh <minhnguyengit@gmail.com>
2018-11-16 16:59:10 +0700

    80% function revemie

--- src/component/am_account.jsx
+++ src/component/am_account.jsx
@@ -1,34 +1,62 @@
 import React, { Component } from 'react'
-import { Col, Row } from 'reactstrap'
+import { Col, Row, Badge } from 'reactstrap'
 import Avatarimg from '../upload/images/avatar.jpg'
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle
 } from 'reactstrap'
+import { api } from '../config'
+import axios from 'axios'
 
 class AmAccount extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { account: null }
+    this.getInformationUser = this.getInformationUser.bind(this)
+  }
+  componentDidMount() {
+    this.getInformationUser()
+  }
+  getInformationUser() {
+    let self = this
+    let _id = '5bd2de667496b64ea0b41682'
+    axios.get(api.local + '/api/user' + _id)
+      .then(response => {
+        if (response.data.status === 200) {
+          self.setState({ account: response.data.account })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
   render() {
+    let { account } = this.state
+    if (account === null) {
+      return <h1><Badge color="warning" pill>Loading</Badge></h1>
+    }
     return (
       <Row style={{ background: 'white', padding: 20, boxShadow: '1px 1px 11px 6px #888888' }}>
         <Card md={5} sm={5} xs={5} style={{ background: '#5a96da' }}>
           <CardImg style={{ borderRadius: '50%' }} top width='100%' src={Avatarimg} alt='avatar' />
-          <CardBody>
-            <CardTitle>Gia Cac Luong</CardTitle>
-            <CardSubtitle>UserName: JudasFate</CardSubtitle>
+          {account && <CardBody>
+            <CardTitle>{account.infoUser.fullname}</CardTitle>
+            <CardSubtitle>UserName: {account.username}</CardSubtitle>
             <br />
             <CardText>
-              Email: judasfate@gmail.com
+              Email: {account.email}
             </CardText>
             <CardText>
-              Group: Nhi store's Staff
+              Group: {account.groups[0].name}
             </CardText>
             <CardText>
-              Phone: 0937740808
+              Phone: {account.infoUser.phonenumber}
             </CardText>
             <CardText>
-              Hometown: Cai Lay, Tien Giang
+              Hometown: {account.infoUser.hometown}
             </CardText>
           </CardBody>
+          }
         </Card>
         <Col md={7} xs={7} sm={7}>
           {/* something inside */}
--- src/component/am_rn_table.jsx
+++ src/component/am_rn_table.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
-import { Table } from 'reactstrap'
+import { Table, Badge } from 'reactstrap'
+import axios from 'axios'
+import { api } from '../config'
 
-export default class AmrnTable extends React.Component {
-  render () {
+class AmrnTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { bills: null }
+    this.getListBills = this.getListBills.bind(this)
+  }
+  componentDidMount() {
+    this.getListBills()
+  }
+  getListBills() {
+    let id_store = '5bd2de667496b64ea0b41685'
+    let self = this
+    axios.get(api.local + '/api/bills' + id_store)
+      .then(response => {
+        if (response.data.status === 200) {
+          self.setState({ bills: response.data.bills })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  render() {
+    if (this.state.bills === null) {
+      return <h1><Badge color='warning' pill>Loading</Badge></h1>
+    }
+    let arrayTotal = []
+    for (let item of this.state.bills) {
+      let sumbilldetail = 0
+      for (let subitem of item.billDetails) {
+        sumbilldetail = sumbilldetail + parseInt(subitem.price) * parseInt(subitem.quantity)
+      }
+      arrayTotal.push(sumbilldetail)
+    }
+    console.log(arrayTotal)
     return (
       <Table hover>
         <thead>
           <tr>
             <th>#</th>
+            <th>Code</th>
             <th>Bill</th>
             <th>Date</th>
             <th>Author</th>
@@ -15,26 +51,22 @@ export default class AmrnTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope='row'>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope='row'>2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope='row'>3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {this.state.bills.map((item, index) =>
+            <tr key={index}>
+              <th scope='row'>{index + 1}</th>
+              <td><Badge color='info'>{item.code}</Badge></td>
+              <td>{item.title}</td>
+              <td>{item.date}</td>
+              <td>{item.user}</td>
+              <td>{arrayTotal[index]}</td>
+            </tr>
+          )
+          }
         </tbody>
       </Table>
     )
   }
 }
+
+
+export default AmrnTable
