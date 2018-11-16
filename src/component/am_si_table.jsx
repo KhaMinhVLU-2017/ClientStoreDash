b9c6269e23a76c791b1ced77dc310c0ccc224759
minh <minhnguyengit@gmail.com>
2018-11-16 13:36:33 +0700

    Complete Get List User of Group

--- src/component/am_si_table.jsx
+++ src/component/am_si_table.jsx
@@ -1,41 +1,61 @@
 import React from 'react'
 import { Table } from 'reactstrap'
+import axios from 'axios'
+import { api } from '../config'
 
 export default class AmsiTable extends React.Component {
-  render () {
-    return (
-      <Table hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Avatar</th>
-            <th>role</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope='row'>1</th>
-            <td>Luu Gia Kho</td>
-            <td>glc@gmail.com</td>
-            <td>Avatar</td>
-            <td>Employee</td>
-          </tr>
-          <tr>
-            <th scope='row'>2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope='row'>3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-    )
+  constructor(props) {
+    super(props)
+    this.state = { list: [] }
+    this.getListAccount = this.getListAccount.bind(this)
+  }
+  componentDidMount() {
+    this.getListAccount()
+  }
+  getListAccount() {
+    let id_group = '5bd2de667496b64ea0b41684'
+    let self = this
+    axios.get(api.local + '/api/GroupUsers' + id_group)
+      .then(response => {
+        // console.log(response)
+        if (response.data.status === 200) {
+          self.setState({ list: response.data.listdata })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  render() {
+    if (this.state.list.length === 0) {
+      return (<h1>Found user in group</h1>)
+    } else {
+      return (
+        <Table hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.list.map((item, index) => {
+                return (<tr>
+                  <th scope='row'>{index + 1}</th>
+                  <td>{item.username}</td>
+                  <td>{item.email}</td>
+                  <td>{item.role.name}</td>
+                  <td>X</td>
+                </tr>)
+              })
+            }
+          </tbody>
+        </Table>
+      )
+    }
   }
 }
